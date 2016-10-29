6bef871b7846626c1e467d9c09fbc7b4a74cb5db
carolinachm <carolti2013@gmail.com>
2016-10-29 17:35:32 -0200

    Criação das classes carroTeste e cidadeTeste

---
+++ src/br/com/fabricadeprogramador/aula1/CarroTeste.java
@@ -0,0 +1,34 @@
+package br.com.fabricadeprogramador.aula1;
+
+/**
+ * Created by carolina on 29/10/16.
+ */
+public class CarroTeste {
+
+    public static void main(String[] args) {
+
+        Carro carro = new Carro();
+        carro.ano = 2016;
+        carro.cor = "vermelho";
+        carro.marca = "fiat";
+        carro.modelo = "fusion";
+
+        Carro  carro1 = new Carro();
+        carro1.ano = 2017;
+        carro1.cor = "branco";
+        carro1.marca = "chevrolet";
+        carro1.modelo = "captiva";
+
+        Carro  carro2 = new Carro();
+        carro2.ano = 2016;
+        carro2.cor = "branco";
+        carro2.marca = "hiuday";
+        carro2.modelo = "elantra";
+
+        System.out.println(" Ano:" + carro.ano + ", Cor:" + carro.cor + ", Marca:" + carro.marca + ", Modelo:" + carro.modelo);
+        System.out.println("");
+        System.out.println(" Ano:" + carro1.ano + ", Cor:" + carro1.cor + ", Marca:" + carro1.marca + ", Modelo:" + carro1.modelo);
+        System.out.println("");
+        System.out.println(" Ano:" + carro2.ano + ", Cor:" + carro2.cor + ", Marca:" + carro2.marca + ", Modelo:" + carro2.modelo);
+    }
+}
---
+++ src/br/com/fabricadeprogramador/aula1/CidadeTeste.java
@@ -0,0 +1,30 @@
+package br.com.fabricadeprogramador.aula1;
+
+/**
+ * Created by carolina on 29/10/16.
+ */
+public class CidadeTeste {
+
+
+        public static void main(String[] args) {
+
+
+            Cidade cidade = new Cidade();
+            cidade.nome = "Valparaiso de Goias";
+            cidade. uf = "GO";
+
+            Cidade cidade1 = new Cidade();
+            cidade1.nome = "Taguatinga";
+            cidade1. uf = "DF";
+
+            Cidade cidade2 = new Cidade();
+            cidade2.nome = "Asa Norte";
+            cidade2. uf = "DF";
+
+            System.out.println("Nome:" + cidade.nome+ ", UF:" + cidade. uf );
+            System.out.println("");
+            System.out.println("Nome:" + cidade1.nome+ ", UF:" + cidade1. uf );
+            System.out.println("");
+            System.out.println("Nome:" + cidade2.nome+ ", UF:" + cidade2. uf );
+        }
+    }
