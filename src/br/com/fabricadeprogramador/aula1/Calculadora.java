62b273eef345148f3bec8feccb28f63ab774c8a6
carolinachm <carolti2013@gmail.com>
2016-10-30 21:49:55 -0200

    enumeracao

---
+++ src/br/com/fabricadeprogramador/aula1/Calculadora.java
@@ -0,0 +1,30 @@
+package br.com.fabricadeprogramador.aula1;
+
+/**
+ * Created by carolina on 30/10/16.
+ */
+public class Calculadora {
+
+    public int a;
+    public int b;
+
+
+
+
+    public int somar(int a, int b){
+        return a + b;
+    }
+    public int subtrair(int a, int b){
+        return a - b;
+    }
+    public int multiplicar(int a, int b){
+        return a * b;
+    }
+    public double dividir(double a, double b){
+        return a / b;
+    }
+    public double mode(double a, double b){
+        return a % b;
+    }
+
+}
---
+++ src/br/com/fabricadeprogramador/aula1/EstadoCivil.java
@@ -0,0 +1,14 @@
+package br.com.fabricadeprogramador.aula1;
+
+/**
+ * Created by carolina on 30/10/16.
+ */
+public enum EstadoCivil {
+
+    SOLTEIRO,
+    CASADO,
+    VIUVO,
+    DIVORCIADO;
+
+
+}
